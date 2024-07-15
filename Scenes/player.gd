0b7b026502e895a095b11d2af0417f1c1e599e37
Robin Sizemore <robin.b.sizemore@outlook.com>
2024-07-15 14:03:22 -0400

    Initial Commit

---
+++ Scenes/player.gd
@@ -0,0 +1,12 @@
+extends RigidBody3D
+
+@export var move_speed: float = 2.0
+
+func _physics_process(delta):
+	if Input.is_key_pressed(KEY_LEFT):
+		linear_velocity.x = -move_speed
+	elif Input.is_key_pressed(KEY_RIGHT):
+		linear_velocity.x = move_speed
+	else:
+		linear_velocity.x = 0
+	
